96a0ca1a2aae423c5eeff2a91b47283394b4609c
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-11-06 21:09:28 +0200

    feat: create with padding & with border stories

---
+++ src/components/template/WithBorder.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { WithBorderProps } from '../../types/template/TemplateProps';
+
+const borderTypes = {
+    all: { border: "0.0625rem solid rgba(224, 224, 224, 1)" },
+    bottom: { borderBottom: "0.0625rem solid rgba(224, 224, 224, 1)" },
+    top: { borderTop: "0.0625rem solid rgba(224, 224, 224, 1)" }
+}
+
+function WithBorder(props: WithBorderProps): React.ReactElement {
+    const { children, type } = props;
+
+    return (
+        <div
+            data-ktest="workinglists-border"
+            style={borderTypes[type]}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default WithBorder
