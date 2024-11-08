b09c4d454d7d6de12135cc5d8c7af8d2e3e86044
EdsonNhancale <edsonnhancale97@gmail.com>
2024-11-08 10:30:17 +0200

    fix: non showing components

--- src/stories/table/RowsCell.stories.ts
+++ src/stories/table/RowsCell.stories.ts
@@ -1,28 +1,19 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
 import { fn } from '@storybook/test';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-select/dist/react-select.css";
-import { Button } from '../button/Button';
+import RowCell from '../../components/table/components/row/RowCell';
+
 
 const meta = {
   title: 'Table/RowsCell',
-  component: Button,
+  component: RowCell,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof RowCell>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const PrimaryRowsCell: Story = {
-  args: {
-    primary: true,
-    label: 'RowsCell Button',
-  },
-};
