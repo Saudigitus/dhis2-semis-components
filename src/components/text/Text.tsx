c300371c9c2a352783094e233a2630f1918a3af2
cnandza <clayd.nandza@saudigitus.org>
2024-11-08 11:20:05 +0200

    SEMISC-8 feat(TEXT): add title and subtitke components

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -4,9 +4,9 @@ import styles from "./text.module.css";
 import { TextProps } from "../../types/text/TextProps";
 
 function Text(props: TextProps): React.ReactElement {
-  const { label, type } = props;
+  const { label, type, size = "bold" } = props;
 
-  return <label className={classNames(styles.text, styles[`${type}`])}>{label}</label>;
+  return <label className={classNames(styles.text, styles[`${type}`], styles[`${size}`])}>{label}</label>;
 }
 
 export default Text;
--- src/types/text/TextProps.ts
+++ src/types/text/TextProps.ts
@@ -1,6 +1,7 @@
 interface TextProps {
     label: string
     type: "title" | "subtitle"
+    size?: "normal" | "bold"
 }
 
 export type { TextProps }
