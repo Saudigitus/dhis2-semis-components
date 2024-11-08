cab42364a31c549f43f7ef110f177666e211ca96
cnandza <clayd.nandza@saudigitus.org>
2024-11-08 15:58:23 +0200

    SEMISC-3 enhance modal component and use botton to control it on stories

--- src/types/modal/ModalProps.ts
+++ src/types/modal/ModalProps.ts
@@ -1,11 +1,18 @@
-
 interface ModalProps {
+    /** The title that appears in the modal. */
     title: string
+    /** The variable which controls the modal status. If true the modal is opened. */
     open: boolean
-    size?: "small" | "medium" | "large"
-    position?: "top" | "middle" | "bottom"
+    /** This variable determines if the modal will be closed with click away. */
+    isClickAway?: boolean
+    /** To set the open value to false. */
+    handleClose: () => void
+    /** The modal body content. */
     children: React.ReactNode
-    setOpen: (value: boolean) => void
+    /** To set modal width. Default - large.  */
+    size?: "small" | "medium" | "large"
+    /** To set modal position on app window. Default - middle.*/
+    position?: "top" | "middle" | "bottom"
 }
 
 export type { ModalProps }
