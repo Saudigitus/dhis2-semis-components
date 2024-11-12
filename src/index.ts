c13ac23ed06f5e3eaf62ae45cbc6590542826f97
EdsonNhancale <edsonnhancale97@gmail.com>
2024-11-12 16:09:10 +0200

    feat(Index): added all components in index.ts file to be exported

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { DashboardCard, SummaryCard } from "./components"
+import DropZone from "./components/dropzone/DropZone"
+import GroupForm from "./components/form/GroupForm"
+import SingleSelectField from "./components/genericFields/fields/SingleSelect"
+import MainHeader from "./components/header/mainHeader"
+import InfoPage from "./components/info/InfoPage"
+import { SideBar } from "./components/layout"
+import ModalComponent from "./components/modal/Modal"
+import Pagination from "./components/table/components/pagination/Pagination"
+import TableRowActions from "./components/table/components/rowsActions/TableRowActions"
+import RenderHeader from "./components/table/render/RenderHeader"
+import RenderRows from "./components/table/render/RenderRows"
+import Table from "./components/table/render/Table"
+import WithBorder from "./components/template/WithBorder"
+import WithPadding from "./components/template/WithPadding"
+import Text from "./components/text/Text"
+
+
+export {
+    Table,
+    SingleSelectField,
+    GroupForm,
+    DashboardCard,
+    SummaryCard,
+    DropZone,
+    RenderHeader,
+    RenderRows,
+    Pagination,
+    TableRowActions,
+    MainHeader,
+    InfoPage,
+    ModalComponent,
+    SideBar,
+    Text,
+    WithBorder,
+    WithPadding
+}
