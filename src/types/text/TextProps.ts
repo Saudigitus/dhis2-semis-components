62f1383ed87d0da3ff2bfb8415986989edde470d
cnandza <clayd.nandza@saudigitus.org>
2024-11-14 09:26:25 +0200

    SEMISC-3: documentation

--- src/types/text/TextProps.ts
+++ src/types/text/TextProps.ts
@@ -1,7 +1,19 @@
 interface TextProps {
+    /**
+     * The text to display.
+     * @type {string}
+     */
     label: string
+    /**
+     * The type of text. Each one has a special styles.
+     * @type {("title" | "subtitle")}
+     */
     type: "title" | "subtitle"
-    size?: "normal" | "bold"
+    /**
+     * Font weight.
+     * @type {?("normal" | "bold")}
+     */
+    weight?: "normal" | "bold"
 }
 
 export type { TextProps }
