69a72dff64042f1d5f0a6a3d51ddc6a48c186c36
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-01-10 15:25:19 +0200

    feat: Show option display name instead of code

---
+++ src/types/programStageConfig/ProgramStageConfig.ts
@@ -0,0 +1,26 @@
+import { OptionsProps } from "../variables/AttributeColumns"
+
+interface programStageDataElements {
+    displayInReports: boolean
+    compulsory: boolean
+    dataElement: {
+        displayInReports: boolean | undefined
+        displayName: string
+        id: string
+        valueType: string
+        optionSet: {
+            id: string
+            options: OptionsProps[]
+        }
+    }
+}
+interface ProgramStageConfig {
+    autoGenerateEvent: boolean
+    displayName: string
+    id: string
+    repeatable:boolean,
+    executionDateLabel?: string
+    programStageDataElements: programStageDataElements[]
+}
+
+export type { ProgramStageConfig, programStageDataElements }
---
+++ src/types/utils/FormatRowsDataProps.ts
@@ -0,0 +1,8 @@
+import { ProgramConfig } from "../programConfig/ProgramConfig"
+
+interface defaultProps {
+    metaData: string
+    program: ProgramConfig
+    value: string
+}
+export type { defaultProps }
