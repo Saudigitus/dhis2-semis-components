7920e3851e48237f6a78f885001a469e5e1933f3
cnandza <cnandza@saudigitus.org>
2025-06-23 16:36:50 +0200

    implement bottom sticked menu item on side bar

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -5,19 +5,19 @@ import SibeBarCollapseBtn from './components/SibeBarCollapseBtn';
 import { SideBarItemProps, SideBarProps } from '../../types/sideBar/SideBarTypes';
 
 export default function SideBar(props: SideBarProps): React.ReactElement {
-    const { sideBarData, collapsed: isCollapsed = true, sideBarBtnPosition = "top" } = props;
+    const { sideBarData, collapsed: isCollapsed = true, sideBarBtnPosition = "top", stickLastItemToBottom = false } = props;
     const [collapsed, setCollapsed] = useState<boolean>(isCollapsed);
 
     return (
         <aside className={collapsed ? style.sideBarContainerCollapsed : style.sideBarContainer}>
-            <div className={style.sideBarMenu}>
+            <div className={stickLastItemToBottom ? style.sideBarMenuSticked : style.sideBarMenu}>
                 {
                     sideBarData?.filter((element: SideBarItemProps) => element.displayInMenu).map((element: SideBarItemProps, index: number) => (
                         <SideBarItem key={index} title={element.title} subItems={element.subItems} />
                     ))
                 }
             </div>
-            <SibeBarCollapseBtn collapsed={collapsed} setCollapsed={setCollapsed} sideBarBtnPosition={sideBarBtnPosition}/>
+            <SibeBarCollapseBtn collapsed={collapsed} setCollapsed={setCollapsed} sideBarBtnPosition={sideBarBtnPosition} />
         </aside>
     )
 }
