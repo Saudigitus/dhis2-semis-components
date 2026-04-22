ae6ce5ff0f55489506a9b04138b89bcba522442f
cnandza <cnandza@saudigitus.org>
2026-04-22 10:59:23 +0200

    fix: replace fragment with div in DropDownCalendar

--- src/components/calendar/DropDownCalendar.tsx
+++ src/components/calendar/DropDownCalendar.tsx
@@ -53,6 +53,6 @@ export default function DropDownCalendar(props: DropDownCalendarProps) {
                     </div>
                 </Paper>
             </Popover>
-        </>
+        </div>
     )
 }
