56617482a82279009f18f28712ef16cc2f489836
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2026-08-01 20:08:47 +0200

    feat(dropdown-button): add full width support
    add DropdownButton.module.css with 100% width styling
    update DropdownProps to mark disabled as optional and add fullWidth prop
    apply full width class to SplitButton when prop is provided
    fix missing newline at end of DropdownProps.ts

--- src/components/buttons/dropdown/DropdownButton.tsx
+++ src/components/buttons/dropdown/DropdownButton.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { SplitButton } from "@dhis2/ui";
 import FlyoutMenu from "../../../components/menu/FlyoutMenu";
 import { DropdownProps } from "../../../types/buttons/DropdownProps";
+import styles from './DropdownButton.module.css'
 
 function CustomDropdown(props: DropdownProps): React.ReactElement {
-  const { name, icon, options, disabled } = props;
+  const { name, icon, options, disabled, fullWidth } = props;
 
   return (
     <SplitButton
       icon={icon}
       disabled={disabled}
+      className={fullWidth && styles.dropDown}
       component={<FlyoutMenu options={options} />}
     >
       {name}
