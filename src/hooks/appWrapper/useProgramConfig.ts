6d194f88b763c4a00d15f03fa10495fb02440817
Edson Nhancale <edsonNhancale97@gmail.com>
2026-09-11 12:10:06 +0200

    feat: add GenericFields component and useProgramConfig hook for dynamic form rendering and configuration management

--- src/hooks/appWrapper/useProgramConfig.ts
+++ src/hooks/appWrapper/useProgramConfig.ts
@@ -9,10 +9,11 @@ const PROGRAMQUERY: any = (id: string) => ({
       fields: [
         "access",
         "programIndicators[displayName,id]",
+        "programSections[id,displayName,description,sortOrder,trackedEntityAttributes[id]]",
         "id,displayName,description,programType,version,translations",
         "trackedEntityType[id,trackedEntityTypeAttributes[trackedEntityAttribute[id]]]",
         "programTrackedEntityAttributes[name,displayName,mandatory,searchable,displayInList,trackedEntityAttribute[translations,generated,pattern,id,displayName,name,formName,unique,valueType,optionSet[options[translations,style,code~rename(value),displayName~rename(label)]]]]",
-        "programStages[id,displayName,autoGenerateEvent,repeatable,translations,programStageDataElements[translations,name,displayName,displayInReports,compulsory,dataElement[translations,id,displayName,formName,name,valueType,optionSet[options[translations,style,code~rename(value),displayName~rename(label)]]]]]"
+        "programStages[id,displayName,autoGenerateEvent,repeatable,translations,programStageSections[id,displayName,description,sortOrder,dataElements[id]],programStageDataElements[translations,name,displayName,displayInReports,compulsory,dataElement[translations,id,displayName,formName,name,valueType,optionSet[options[translations,style,code~rename(value),displayName~rename(label)]]]]]"
       ]
     }
   }
